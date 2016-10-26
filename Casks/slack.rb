e5031928cc1f640a4b37484691c19f569466d002
Han Su Kim <hkim823@gmail.com>
2016-10-26 16:29:12 -0400

    Slack 2.3.0

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,10 +1,8 @@
 class Slack < Cask
-  version '0.44.2'
-  sha256 '2b24fa40d0c928bd1cba82263dbb34b12cf9486e7b6dfff5d24216a6b89d4584'
+  version '2.3.0'
+  sha256 '21f8bcef54132e1d67d7e697d9805790f249ef3fe7b3f4f99007d138187d7990'
 
-  url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/2?format=zip&amp;avtoken=b79aac107b7fda6c56fc5af6052bbc1713da870e'
-  appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
-          :sha256 => '0bfbef89c2b420d7485baf482cdd82d84f5e529c92cca653996e15ca5e6ba3c6'
+  url 'https://downloads.slack-edge.com/mac_releases/Slack-2.3.0-macOS.zip''
   homepage 'http://slack.com'
   license :unknown
 
